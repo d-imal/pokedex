db5707dfba60de3edca20620a71ada5a3145b469
Daniel Imal <mrdanimal@gmail.com>
2023-05-27 15:24:48 -0400

    render more data

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -7,13 +7,12 @@ const SearchField: React.FC = () => {
   const [trigger, { data, error, isLoading }] = pokemonApi.useLazyGetPokemonByNameQuery(); // useGetPokemonByNameQuery('miraidon');
   const [searchTerm, setSearchTerm] = useState<string>('');
 
-  const handleOnClick = (e) => {
+  const handleOnClick = (e: React.FormEvent) => {
     e.preventDefault();
 
     setSearchTerm(searchTerm);
     trigger(searchTerm);
   };
-  console.log({ error });
 
   return (
     <div className={classes.root}>
@@ -35,7 +34,23 @@ const SearchField: React.FC = () => {
       <main className={classes.result}>
         {error && <p>Oh no, there was an error</p>}
 
-        {data && !error && <h2>{data.name}</h2>}
+        {data?.name && !error && (
+          <>
+            <h2>{data.name}</h2>
+            <dl>
+              <dt>Name:</dt>
+              <dd>{data.name}</dd>
+              <dt>Height:</dt>
+              <dd>{data.height}</dd>
+              <dt>Weight:</dt>
+              <dd>{data.weight}</dd>
+              <dt>Abilities:</dt>
+              {data.abilities?.map((ability) => (
+                <dd key={ability.ability.name}>{ability.ability.name}</dd>
+              ))}
+            </dl>
+          </>
+        )}
       </main>
     </div>
   );
