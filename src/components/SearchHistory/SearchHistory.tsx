7e5a3b344e36febde8d02314f931fb879851b166
Daniel Imal <mrdanimal@gmail.com>
2023-05-28 16:03:07 -0400

    render history

--- src/components/SearchHistory/SearchHistory.tsx
+++ src/components/SearchHistory/SearchHistory.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
 import styles from './SearchHistory.module.css';
+import { useSelector } from 'react-redux';
+import { IRootState } from '../../store/Store';
 
 interface ISearchHistoryProps {}
 
 const SearchHistory: React.FC<ISearchHistoryProps> = (props) => {
-  return <div className={styles.root}>SearchHistory</div>;
+  const { history } = useSelector((state: IRootState) => state.search);
+  return (
+    <div className={styles.root}>
+      SearchHistory
+      <ul>
+        {history.reverse().map((term, i) => (
+          <li key={i}>{term}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default SearchHistory;
--- src/store/Store.ts
+++ src/store/Store.ts
@@ -4,6 +4,8 @@ import { setupListeners } from '@reduxjs/toolkit/query';
 import { pokemonApi } from './reducers/PokemonApi';
 import searchReducer from './reducers/Search';
 
+export type IRootState = ReturnType<typeof store.getState>;
+
 export const store = configureStore({
   reducer: {
     [pokemonApi.reducerPath]: pokemonApi.reducer,
