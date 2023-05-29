ec22b724fd3d20489259d24d4d5f31b2aacb06af
Daniel Imal <mrdanimal@gmail.com>
2023-05-28 20:19:09 -0400

    cleanup

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { IRootState } from '../../store/Store';
 import { pokemonApi } from '../../store/reducers/PokemonApi';
 import { setSearchTerm as persistSearchTerm, pushTermToHistory } from '../../store/reducers/Search';
 
 import classes from './SearchField.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { IRootState } from '../../store/Store';
 
 const SearchField: React.FC = () => {
   const dispatch = useDispatch();
   const [trigger, { data, error }] = pokemonApi.useLazyGetPokemonByNameQuery();
   const [searchTerm, setSearchTerm] = useState<string>('');
+  const persistedSearchTerm = useSelector((state: IRootState) => state.search.searchTerm);
 
   const handleOnClick = (e: React.FormEvent) => {
     e.preventDefault();
@@ -19,14 +21,10 @@ const SearchField: React.FC = () => {
     trigger(searchTerm);
   };
 
-  const persistedSearchTerm = useSelector((state: IRootState) => state.search.searchTerm);
-
   useEffect(() => {
     setSearchTerm(persistedSearchTerm);
     trigger(persistedSearchTerm);
   }, [persistedSearchTerm, trigger]);
-
-  console.log({ persistedSearchTerm });
 
   return (
     <div className={classes.root}>
