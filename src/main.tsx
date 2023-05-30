4e28bfd91a71214e2d53efa52d2c695441843ce9
Daniel Imal <mrdanimal@gmail.com>
2023-05-30 15:44:08 -0400

    order imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from './store/Store.ts';
 import { Provider } from 'react-redux';
+
+import { store } from './store/Store.ts';
 
 import App from './components/App/App.tsx';
 
